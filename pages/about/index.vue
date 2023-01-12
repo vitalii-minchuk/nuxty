a8438ca354723193cf6bc2bfc377e1d19f6ca4dc
vitalii-minchuk <mcmin4doutsch@gmail.com>
2023-01-12 19:05:41 +0200

    date picker

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -1,10 +1,19 @@
 <template>
-  <div>
-    about page
-  </div>
+    <v-row justify="center">
+      <v-date-picker v-model="date" :allowed-dates="allowedMonths" type="month" class="mt-4" min="2017-06"
+        max="2019-10"></v-date-picker>
+    </v-row>
 </template>
 <script>
 export default {
-  
+  data() {
+    return {
+      date: '2018',
+    }
+  },
+
+  methods: {
+    allowedMonths: val => parseInt(val.split('-')[1], 10) % 2 === 0,
+  },
 }
 </script>
